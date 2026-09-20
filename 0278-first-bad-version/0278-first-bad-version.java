c6d6a900bdfa12de296243adb9b731c46f41fbb5
Shivaputra Desai <shivaputradesai3184@gmail.com>
2026-09-20 13:46:07 +0530

    Time: 13 ms (99.82%), Space: 42 MB (54.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -4,16 +4,12 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int i=0,j=n,mid=0;
-        if(j==1)
-            return 1;
         while(i<=j){
             mid=i+(j-i)/2;
             if(isBadVersion(mid))
                 j=mid-1;
             else
                 i=mid+1;
-            
-
         }
         return i;
         
